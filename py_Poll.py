cb4abdabe600b81d2662a4bf90db7a7185e5fe9f
Michelle Demars <thenotfrenchgirl@gmail.com>
2022-04-16 10:07:10 -0500

    Updating at 3.6.1 with text file mods

--- py_Poll.py
+++ py_Poll.py
@@ -51,48 +51,58 @@ with open(file_to_load) as election_data:
             candidate_votes[candidate_name] = 0
         # Add a vote to that candidate's count
         candidate_votes[candidate_name] += 1
-       
+
+# Save the results to our text file
+with open(file_to_save, 'w') as txt_file:    
+    # Print the final vote count to the terminal.
+    election_results = (
+        f'\nElection Results\n'
+        f'--------------------*-*-\n'
+        f'Total Votes: {total_votes:,}\n'
+        f'--------------------*-*-')  
+    print(election_results, end='')
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
+
+    # Determine the pecentage of votes for each candidate by looping through the counts.
+    # 1. Iterate through the candidate list
+    for candidate_name in candidate_votes:
+        # 2. Retrieve vote count of a candidate
+        votes = candidate_votes[candidate_name]
+        # 3. Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+        # ASK DURING OFFICE HOURS WHY THIS IS ONLY WORKING WITH THE PLAY BUTTON THIS IS STUPID
+        print(f'{candidate_name}: received {vote_percentage:.2f}% of the vote.')
+        # print('CODE TEST')
     
-# Determine the pecentage of votes for each candidate by looping through the counts.
-# 1. Iterate through the candidate list
-for candidate_name in candidate_votes:
-    # 2. Retrieve vote count of a candidate
-    votes = candidate_votes[candidate_name]
-    # 3. Calculate the percentage of votes.
-    vote_percentage = float(votes) / float(total_votes) * 100
-     # ASK DURING OFFICE HOURS WHY THIS IS ONLY WORKING WITH THE PLAY BUTTON THIS IS STUPID
-    
-    # print(f'{candidate_name}: received {vote_percentage:.2f}% of the vote.')
-    # print('CODE TEST')
-     #Determine winning vote count and candidates
-    # 1. Determine if the votes are greater than the winning count.
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        # 2 If true then set winning_count = votes and winning_percentage = vote_percentage
-        winning_count = votes
-        winning_percentage = vote_percentage
-        # 3 Set the winning_candidate equal to the candidates name.
-        winning_candidate = candidate_name
-        # TO DO:  Print the candidate name and percentage of votes to the terminal
-    print(f'{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n')
-winning_candidate_summary = (f'-*-*---------------------*-*-\n' f'Winner: {winning_candidate}\n' f'Winning Vote Count: {winning_count:,}\n' f'Winning Percentage: {winning_percentage:.1f}%\n' f'-*-*---------------------*-*-\n')
-print(winning_candidate_summary)
+        #Determine winning vote count and candidates
+        # 1. Determine if the votes are greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # 2 If true then set winning_count = votes and winning_percentage = vote_percentage
+            winning_count = votes
+            winning_percentage = vote_percentage
+            # 3 Set the winning_candidate equal to the candidates name.
+            winning_candidate = candidate_name
+            # TO DO:  Print the candidate name and percentage of votes to the terminal
+        # print(f'{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n')
+    winning_candidate_summary = (f'-*-*---------------------*-*-\n' f'Winner: {winning_candidate}\n' f'Winning Vote Count: {winning_count:,}\n' f'Winning Percentage: {winning_percentage:.1f}%\n' f'-*-*---------------------*-*-\n')
+    # print(winning_candidate_summary)
 
-# print('serial killer run run run away')
-# Print the candidate vote dictionary
-# print(candidate_votes)
+    # print('serial killer run run run away')
+    # Print the candidate vote dictionary
+    # print(candidate_votes)
 
-# Print the candidate list
-# print(candidate_options)
+    # Print the candidate list
+    # print(candidate_options)
 
-# Print the total votes.
-# print(total_votes)
+    # Print the total votes.
+    # print(total_votes)
 
 
-# Using the with statement to open the file as a text file
-with open(file_to_save, 'w') as txt_file:
+
 
     # Write three countties to the file.
-    txt_file.write('Counties in the Election\n------------------------\nArapahoe\nDenver\nJefferson')
+    txt_file.write('\nCounties in the Election\n------------------------\nArapahoe\nDenver\nJefferson')
 
 #Close the file
 txt_file.close()
